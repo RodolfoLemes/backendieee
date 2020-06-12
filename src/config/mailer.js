6302f4c0b9602dd5a6ccab508d181e8910ccb302
unknown <rodolfo_fero@hotmail.com>
2020-06-11 22:01:28 -0300

    oi

---
+++ src/config/mailer.js
@@ -0,0 +1,13 @@
+const nodemailer = require('nodemailer')
+
+let transporter = nodemailer.createTransport({
+  host: 'in-v3.mailjet.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: '71393f0a95e9650f081ae54e27ddec2b',
+    pass: '6aada5205ea5e732e9946aee27409201'
+  }
+})
+
+module.exports = transporter
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,12 @@ const routes = express.Router()
 const PostController = require('./controllers/PostController')
 const RamoController = require('./controllers/RamoController')
 const MembroController = require('./controllers/MembroController')
+const transporter = require('./config/mailer')
+
+function isEmail(email) {
+	var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
 
 // Feed
 routes.get('/feed', PostController.getPosts)
@@ -23,4 +29,31 @@ routes.get('', (req, res) => {
     return res.send('bubelezanda')
 })
 
+// Contact
+routes.post('/contact', async (req, res) => {
+    const { email, msg } = req.body
+    
+    if(email !== '' && msg !== '') {
+        if(isEmail(email)) {
+            try {
+                let info = await transporter.sendMail({
+                    from: email, // sender address
+                    to: 'rodolfo_fero@hotmail.com', // list of receivers
+                    subject: "CONTATO", // Subject line
+                    text: msg, // plain text body
+                })
+                if(info) {
+                    console.log("TORBA")
+                }
+            } catch(err) {
+                console.log(err)
+                return res.send(false)
+            }
+        } else {
+            return res.send(false)
+        }
+    }
+
+    return res.send(true)
+})
 module.exports = routes
