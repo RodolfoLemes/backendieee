808d9eb3cc40fc4b89a87e6b1cd4dce9e2458015
unknown <rodolfo_fero@hotmail.com>
2020-06-26 03:30:21 -0300

    vvvvv

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = express.Router()
+const multer = require('multer')
 
+const upload = multer()
 const CourseController = require('../controllers/CourseController')
 
-routes.post('/create', CourseController.create)
+routes.post('/create', upload.any(), CourseController.create)
 
 module.exports = routes
