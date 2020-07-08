9ef611bc223faca5b46bb75376736abb6803689e
unknown <rodolfo_fero@hotmail.com>
2020-07-08 18:23:52 -0300

    foda-se

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -5,6 +5,7 @@ const EventController = require('../controllers/EventController')
 
 routes.get('/', EventController.getList)
 routes.get('/:eventId', EventController.getOne)
+routes.delete('/:eventId', EventController.delete)
 routes.post('/', EventController.create)
 routes.post('/many', EventController.createMany)
 
